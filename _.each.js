acb65c04e450f6a561799c0bc18265c575e5c05b
seanchen1991 <skypemaster007@gmail.com>
2014-04-07 18:06:21 -0700

    Update _.each.js
    _.each calls a function on every value in a collection in turn.

--- _.each.js
+++ _.each.js
@@ -1,12 +1,12 @@
 function _.each(list, iterator, [context]){
-  if (list == null){  		                 //checks if list is empty and returns it if it is
+  if (list == null){  		                   //checks if list is empty and returns it if it is
     return list;
   }
-  if (list.typeOf == 'object'){			 //if list is an object, return the list element associated with each key
+  if (list.typeOf == 'object'){			         //if list is an object, return the list element associated with each key
     for (key in list){
       iterator(list[key], key, list);									
     }
-    else if (list.typeOf == 'array'){		//if list is an array, performs forEach on the list
+    else if (list.typeOf == 'array'){		     //if list is an array, performs forEach on the list
       for (var i = 0; i < list.length; i++){
         iterator(list[i]);
       }
