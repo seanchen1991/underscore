ef1a49bc9bc50eb97f799d405d9f1484f91b2b1f
seanchen1991 <skypemaster007@gmail.com>
2014-04-07 18:04:20 -0700

    Update _.map.js
    _.map calls a function on every value in a collection in turn, returning a collection of the results.

--- _.map.js
+++ _.map.js
@@ -1,12 +1,10 @@
-_.map calls a function on every value in a collection in turn, returning a collection of the results.
-
 function _.map(list, iterator, [context]){
   var array = [];														//initialize an empty
-  if (list == null){												//checks that list is nonempty
-	  return list;
+  if (list == null){				//checks that list is nonempty
+    return list;
   }
-  for (i = 0; i < list.length; i++){				//calls iterator on each list element and pushes each to array
-	  array.push(iterator(list[i]]));
+  for (i = 0; i < list.length; i++){		//calls iterator on each list element and pushes each to array
+    array.push(iterator(list[i]]));
   }
   return array;															//returns the array
 };  
