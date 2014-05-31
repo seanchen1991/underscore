b3b8d09dda17bba65432050c130095484cb9fef7
Sean Chen <skypemaster007@gmail.com>
2014-05-31 16:14:03 -0700

    Update _.reduce.js

--- _.reduce.js
+++ _.reduce.js
@@ -1,8 +1,17 @@
 //_.reduce collects a composite value from the incremental results of a function
 //supplied with an accumulation value and each value in a collection.
 
-function _.reduce(list, iterator, memo, [context]){  //the memo parameter is the initial state of the reduction
-  for (var i = 0; i < list.length; i++){
-    memo = iterator(memo, list[i]);                  //update the memo parameter with each function call on list[i]
-  return memo;
-};
+function _.reduce(list, iterator, memo){  
+  if (list.length > 0 && memo === undefined) {
+    var start = list[0];
+    _.each(list, function(element) {
+      start = iterator(start, element);
+    })
+  } else {
+    var start = memo;
+    _.each(list, function(element) {
+      start = iterator(start, element);
+    })
+  }
+  return start;
+}
