193defe89cc3d7dea59d2cff5ccf3c9861ad23bf
Sean Chen <skypemaster007@gmail.com>
2014-05-31 16:06:28 -0700

    Update _.each.js

--- _.each.js
+++ _.each.js
@@ -1,16 +1,13 @@
 //_.each calls a function on every value in a collection in turn.
 
-function _.each(list, iterator, [context]){
-  if (list == null){  		                     //checks if list is empty and returns it if it is
-    return list;
-  }
-  if (list.typeOf == 'object'){			     //if list is an object, return the list element associated with each key
-    for (key in list){
-      iterator(list[key], key, list);									
+function _.each(list, iterator){
+  if (list.length !== undefined){  		                   
+    for (var i = 0; i < list.length; i++) {
+      iterator(list[i], i, list);
     }
-    else if (list.typeOf == 'array'){		     //if list is an array, performs forEach on the list
-      for (var i = 0; i < list.length; i++){
-        iterator(list[i]);
-      }
-   }
+  } else {
+    for (var key in list) {
+      iterator(list[key], key, list);
+    }
+  }
 };
