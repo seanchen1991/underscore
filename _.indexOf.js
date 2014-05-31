b484783a9618cf128b207e93a473dedbe1ce5997
Sean Chen <skypemaster007@gmail.com>
2014-05-31 16:29:59 -0700

    Update _.indexOf.js

--- _.indexOf.js
+++ _.indexOf.js
@@ -1,3 +1,6 @@
+/*Returns the index at which value can e found in the array, or -1 if the value is not
+present in the array.*/
+
 function _.indexOf(array, target) {
   var result = -1;
   _.each(array, function(item, index) {
