1dda02a2210967f2005a48137f893bd9fdb439db
Sean Chen <skypemaster007@gmail.com>
2014-05-31 16:10:59 -0700

    Update _.map.js

--- _.map.js
+++ _.map.js
@@ -1,12 +1,9 @@
 //_.map calls a function on every value in a collection in turn, returning a collection of the results.
 
-function _.map(list, iterator, [context]){
-  var array = [];														//initialize an empty
-  if (list == null){				//checks that list is nonempty
-    return list;
-  }
-  for (i = 0; i < list.length; i++){		//calls iterator on each list element and pushes each to array
-    array.push(iterator(list[i]]));
-  }
-  return array;															//returns the array
-};  
+function _.map(list, iterator){
+  var array = [];														
+  _.each(list, function(item) {
+    array.push(iterator(item));
+  })
+  return array;
+}
