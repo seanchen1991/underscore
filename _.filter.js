f2800c479a7f35f6635f2a98b9faa41f51af354d
Sean Chen <skypemaster007@gmail.com>
2014-05-31 16:08:27 -0700

    Update _.filter.js

--- _.filter.js
+++ _.filter.js
@@ -1,15 +1,15 @@
 //_.filter calls a predicate function that returns a true or false value and grabs 
 //each value where said predicate returned true, returning them in a new collection.
 
-function _.filter(list, predicate, [context]){  //predicate is a predicate function that returns a boolean when called on list
+function _.filter(list, predicate){  
   var array = [];
   if (list == null){
     return list;
   };
-  for (var i = 0; i < list.length; i++){        //if predicate returns true on list[i], add it to the array
-    if (predicate(list[i])){
-      array.push(predicate(list[i]));
+  _.each(list, function(item) {        
+    if (predicate(item)) {
+      array.push(predicate(item));
     }
-  };
+  })
   return array;
-};
+}
